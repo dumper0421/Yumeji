e37eeb602255ac432fd01ee57e38f0e8cfa92d90
dumper0421 <wooij07@naver.com>
2025-05-12 14:10:42 +0900

    #fix: S1#5 수정, #feat: Shoot 에니메이터
    #5 sorting layer추가, 플레이어 움직임 관리
    
    애니메이션은 추후에 player~lerp에서 코드 합쳐야 함

---
+++ Assets/Script/SceneController/Sequence1Scene5Controller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sequence1Scene5Controller : SceneController
+{
+    protected override void OnStopIntervalReached()
+    {
+        playerAnimator.enabled = true;  
+        playerMoveTestLerp.enabled = true;
+    }
+    void Start()
+    {
+        playerAnimator.SetFloat("DirY", 1);
+        playerAnimator.enabled = false;
+        StartCoroutine(StopPlayer());
+    }
+
+    void Update()
+    {
+        
+    }
+}
