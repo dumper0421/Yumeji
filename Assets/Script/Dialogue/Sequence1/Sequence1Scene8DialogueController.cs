49c4ebcdc3123fb8c302668f03a25ced5383905c
dumper0421 <wooij07@naver.com>
2025-05-14 18:22:30 +0900

    #fit: scene8 ~rabbitroom배치, 대화세팅

---
+++ Assets/Script/Dialogue/Sequence1/Sequence1Scene8DialogueController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public enum S1S8State
+{
+    None
+}
+public class Sequence1Scene8DialogueController : DialogueController<S1S8State>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected override void HandleDialogueEnd(string dialogueId)
+    {
+      
+
+    }
+    protected override void DialogueRunning(string dialogueId)
+    {
+
+    }
+    protected override void HandleOption(string text, string nextId)
+    {
+
+    }
+
+    protected override void OnPuzzleComplete()
+    {
+
+    }
+
+    protected override void TryProgress()
+    {
+
+
+    }
+}
