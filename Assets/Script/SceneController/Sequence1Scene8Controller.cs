3172bcb47f03050aea3e4152b2eae6809b978b7f
dumper0421 <wooij07@naver.com>
2025-05-18 15:46:10 +0900

    #feat: 씬8 배치, 텔레포트, 카메라 완료

---
+++ Assets/Script/SceneController/Sequence1Scene8Controller.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sequence1Scene8Controller : SceneController
+{
+    protected override void OnStopIntervalReached()
+    {
+        ;
+    }
+}
