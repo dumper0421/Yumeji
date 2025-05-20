2df5d92586c669f79287634cf215fa4bcbbf3007
dumper0421 <wooij07@naver.com>
2025-05-20 20:20:38 +0900

    #fix: 씬9 난쟁이레이어, feat: Item Data 설명추가

---
+++ Assets/Script/LayerSetting_child.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
+public class LayerSetting_child : MonoBehaviour
+{
+    private SpriteRenderer _sr;
+
+    void Awake()
+    {
+        _sr = GetComponent<SpriteRenderer>();
+    }
+
+    void LateUpdate()
+    {
+        // transform.position.y 는 자식의 월드 Y 좌표이므로
+        // 부모 계층 구조와 상관없이 항상 올바른 화면상의 높이를 가져옵니다.
+        _sr.sortingOrder = Mathf.RoundToInt(transform.position.y * -1f);
+        // * -100f 는 소수점 차이를 더 세밀하게 반영하기 위해 곱한 예시입니다.
+    }
+}
+
