0f5c1987b05f2f4a561406f87e710ac743501ecc
dumper0421 <wooij07@naver.com>
2026-01-23 22:06:08 +0900

    #feat: 2-2
    -2-2-2 영화 이후 대사 추가

---
+++ Assets/Script/Dialogue/Sequence2/Sequence2Scene2DialogueController1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum S2S2State1
+{
+    None
+}
+public class Sequence2Scene2DialogueController1 : DialogueController<S2S2State1>
+
+{
+
+
+    private ItemData selectedTicket;
+
+    [Header("SFX")]
+    [SerializeField] private AudioClip haruCarSFX;
+
+    [SerializeField] private string StartDialogueId;
+    [SerializeField] private string nextSceneName;
+    private void Start()
+    {
+        if (!string.IsNullOrEmpty(StartDialogueId))
+        {
+            dialogueManager.StartDialogue(StartDialogueId);
+        }
+    }
+    protected override void HandleOption(string text, string nextId)
+    {
+    }
+    protected override void HandleDialogueEnd(string dialogueId)
+    {
+        if (dialogueId == "Car_After_Movie")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(nextSceneName);
+
+        }
+    }
+
+    protected override void TryProgress()
+    {
+    }
+
+    protected override void OnPuzzleComplete() { }
+}
