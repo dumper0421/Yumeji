5569a76147f6688fb5383c2a8b5eebae16b163ca
dumper0421 <wooij07@naver.com>
2026-07-24 23:03:54 +0900

    #fix:6-2수정
    연출트리거수정필요

---
+++ Assets/Script/Dialogue/Sequence6/Sequence6Scene2DialogueController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum S6S2State
+{
+    None
+}
+public class Sequence6Scene2DialogueController : DialogueController<S6S2State>
+{
+
+
+    protected override void ApplyWorldByState()
+    {
+    }
+
+    protected override void HandleDialogueEnd(string dialogueId)
+    {
+    }
+
+    protected override void HandleOption(string text, string nextId)
+    {
+    }
+
+    protected override void OnPuzzleComplete()
+    {
+    }
+
+    protected override void TryProgress()
+    {
+    }
+}
